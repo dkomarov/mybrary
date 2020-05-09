f75cfd16b2e24bfe4ab31e15f78b953a63201b4b
dkomarov <dkomarov@hawk.iit.edu>
2020-05-09 18:37:46 -0500

    configure server to connect to local mongoDB
    using local environment variables stored in .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load()
+if (process.env.NODE_ENV !== 'production') { // set by default by Node
+  require('dotenv').config({path: '.env'})
 }
 
 const express = require('express')
@@ -9,13 +9,13 @@ const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views') // __dirname is cwd, views are server-rendered views
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static('public')) // client-side files
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
