b938cb26c008460c57acabbdcaa9d4d806769ebd
dkomarov <dkomarov@hawk.iit.edu>
2020-05-09 22:27:55 -0500

    create & fill-out author route
    allows creating new authors, saves them to mongoDB, and accepts search queries to find matching authors

---
+++ routes/authors.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i') // case in-sensitive
+  }
+  try { 
+    const authors = await Author.find(searchOptions) // find all authors ({})
+    res.render('authors/index', { authors: authors, 
+                                  searchOptions: req.query } )
+  }
+  catch {
+    res.redirect('/')
+  }
+
+});
+
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
+});
+
+// Create Author Route
+router.post('/', async (req, res) => {
+  //res.redirect('author')
+  //res.send(req.body.name)
+  //res.redirect('authors')
+  
+  const author = new Author({
+    name: req.body.name
+  })
+
+  try {
+    const newAuthor = await author.save()
+    res.redirect(`authors/${newAuthor.id}`)
+
+    res.redirect('authors')
+  } catch {
+      let locals = {errorMessage: 'Error creating Author'}
+      res.render('authors/new', {
+        author: author,
+        locals
+        //locals
+      })
+  }
+ });
+//   author.save((err, newAuthor) => {
+//     if (err) {
+//       console.log("Error is:", err.message)
+
+//     } else {
+//       
+//     }
+//   })
+
+
+module.exports = router; // indexRouter 
